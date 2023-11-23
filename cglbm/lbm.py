aeaa1128e600c771bf0b78998a7f0451572ad81d
Mark1626 <nimalan.m@protonmail.com>
2023-11-23 19:16:13 +0530

    Nimalan/Ravinder: Optimize and add eq dist fn

--- cglbm/lbm.py
+++ cglbm/lbm.py
@@ -11,3 +11,21 @@ def eq_dist_phase_field(cXYs, weights, phi, u):
     cu = jnp.sum(u * cXYs)
     u2 = jnp.sum(jnp.square(u))
     return weights * phi * (1.0 - 1.5 * u2 + 3.0 * cu + 4.5 * cu * cu)
+
+
+@jit
+def eq_dist(cXYs, weights, phi_weights, pressure, u):
+    cu = jnp.sum(u * cXYs)
+    u2 = jnp.sum(jnp.square(u))
+    
+    neq_common_term = phi_weights + (weights + 3.0 * pressure) - (weights * u2 * 1.5)
+    neq_k_zero_term = -3.0 * pressure
+    neq_k_nonzero_term = weights * (3.0 * cu + 4.5 * cu * cu)
+    
+    k = jnp.arange(9)
+    neq = neq_common_term + jnp.where(k == 0, neq_k_zero_term, neq_k_nonzero_term)
+    
+    return neq
+
+_eq_dist = vmap(eq_dist, in_axes=(None, None, None, 0, 0), out_axes=1)
+grid_eq_dist = jit(vmap(_eq_dist, in_axes=(None, None, None, 0, 0), out_axes=1))
--- cglbm/lbm_test.py
+++ cglbm/lbm_test.py
@@ -3,10 +3,14 @@ from absl.testing import parameterized
 import jax
 from jax import numpy as jnp
 import cglbm.test_utils as test_utils
-from cglbm.lbm import eq_dist_phase_field
+from cglbm.lbm import eq_dist_phase_field, eq_dist, grid_eq_dist
 
 
 class LBMTest(absltest.TestCase):
+    """Performance test for LBM Methods
+    Note:These are for reference actual numbers will go down after fusion
+    """
+
     def test_eq_dist_phase_field_perf(self):
         sys = test_utils.load_config("params.ini")
 
@@ -25,6 +29,46 @@ class LBMTest(absltest.TestCase):
             return eq_dist_phase_field(sys.cXYs, sys.weights, state["phi"], state["u"])
 
         test_utils.benchmark("benchmark eq dist phase field", init_fn, step_fn)
+
+    
+    def test_grid_eq_dist_perf(self):
+        sys = test_utils.load_config("params.ini")
+
+        def init_fn(rng):
+            LX = sys.LX
+            LY = sys.LY
+            rng1, rng2 = jax.random.split(rng, 2)
+            phi = jax.random.normal(rng1, (LX, LY))
+            u = jax.random.normal(rng2, (LX, LY, 2))
+            return {
+                "phi": phi,
+                "u": u
+            }
+
+        def step_fn(state):
+            return grid_eq_dist(sys.cXYs, sys.weights, sys.phi_weights, state["phi"], state["u"])
+
+        test_utils.benchmark("benchmark grid eq dist", init_fn, step_fn)
+
+    
+    def test_eq_dist_perf(self):
+        sys = test_utils.load_config("params.ini")
+
+        def init_fn(rng):
+            LX = sys.LX
+            LY = sys.LY
+            rng1, rng2 = jax.random.split(rng, 2)
+            phi = jax.random.normal(rng1, (1,))
+            u = jax.random.normal(rng2, (2,))
+            return {
+                "phi": phi,
+                "u": u
+            }
+
+        def step_fn(state):
+            return eq_dist(sys.cXYs, sys.weights, sys.phi_weights, state["phi"], state["u"])
+
+        test_utils.benchmark("benchmark eq dist", init_fn, step_fn)
 
 
 if __name__ == "__main__":
